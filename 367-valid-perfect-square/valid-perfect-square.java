ba4299748295ec10426a7156e698d5728ec2ad10
Sringesh.v <reachsringesh@gmail.com>
2026-09-16 21:06:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (74.30%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -6,10 +6,19 @@ class Solution {
         if(num==0){
             return true;
         }
-        int i;
-        for(i =1;i<=num/i;i++){
-            if(i*i==num){
+        int left =1;
+        int right =num;
+        while(left<=right){
+            int mid = left +(right-left)/2;
+            long squre = (long)mid*mid;
+            if(squre==num){
                 return true;
+            }
+            if(squre>num){
+                right=mid-1;
+            }
+            if(squre<num){
+                left=mid+1;
             }
         }
         return false;
